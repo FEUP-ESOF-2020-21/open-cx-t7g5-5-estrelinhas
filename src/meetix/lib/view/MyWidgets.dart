7328d43ba148646ba6dc45b8060e86d0b7ec7c48
andrenasx <andre2000.an@gmail.com>
2020-11-10 13:16:48 +0000

    MultiSelectChip Widget

--- src/meetix/lib/view/MyWidgets.dart
+++ src/meetix/lib/view/MyWidgets.dart
@@ -94,5 +94,51 @@ class AvatarWithBorder extends StatelessWidget {
       ),
     );
   }
+}
 
+// with love, from https://github.com/ponnamkarthik/MultiSelectChoiceChip/blob/master/lib/main.dart
+class MultiSelectChip extends StatefulWidget {
+  final List<String> list;
+  final Function(List<String>) onSelectionChanged;
+
+  MultiSelectChip(this.list, {this.onSelectionChanged});
+
+  @override
+  _MultiSelectChipState createState() => _MultiSelectChipState();
+}
+
+class _MultiSelectChipState extends State<MultiSelectChip> {
+  // String selectedChoice = "";
+  List<String> selectedChoices = List();
+
+  _buildChoiceList() {
+    List<Widget> choices = List();
+
+    widget.list.forEach((item) {
+      choices.add(Container(
+        padding: const EdgeInsets.all(2.0),
+        child: ChoiceChip(
+          label: Text(item),
+          selected: selectedChoices.contains(item),
+          onSelected: (selected) {
+            setState(() {
+              selectedChoices.contains(item)
+                  ? selectedChoices.remove(item)
+                  : selectedChoices.add(item);
+              widget.onSelectionChanged(selectedChoices);
+            });
+          },
+        ),
+      ));
+    });
+
+    return choices;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: _buildChoiceList(),
+    );
+  }
 }
